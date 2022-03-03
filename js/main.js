cb739eae84bbb4067f5aaebcc0e5d5f8f39a2034
Vitto2623 <vtr.1@icloud.com>
2022-03-03 19:53:34 +0100

    file main.js

--- js/main.js
+++ js/main.js
@@ -1,21 +1,30 @@
 // chiedo di inserire una parola e verifico se è palindroma
 const parolaUtente = prompt("Inserisci una parola");
-let parolaDivisa = parolaUtente.split('');
-parolaDivisa = parolaDivisa.reverse();
-let parolaInvertita = parolaDivisa.join('');
+//let parolaDivisa = parolaUtente.split('');
+//parolaDivisa = parolaDivisa.reverse();
+//let parolaInvertita = parolaDivisa.join('');
 
+let verifica = verificoParolaPalindroma(parolaUtente);
 let outPutParola = document.getElementById("output");
+if (verifica == "si"){
+    outPutParola.innerHTML += " La parola è palindroma."
+}else {
+    outPutParola.innerHTML += " La parola non è palindroma.";
+}
 
-
-verificoParolaUtente(parolaInvertita);
-
-function verificoParolaUtente(parola){
-    if (parolaUtente == parolaInvertita){
-        outPutParola.innerHTML += " La parola è palindroma.";
+//verifica parola palindroma (restituisce "si" oppure "no")
+function verificoParolaPalindroma(parola){
+    let parolaDivisa = parola.split('');
+    parolaDivisa = parolaDivisa.reverse('');
+    let parolaInvertita = parolaDivisa.join('');
+    let risultatoPalindroma;
+    if (parola == parolaInvertita){
+        risultatoPalindroma = "si";
     }else {
-        outPutParola.innerHTML += " La parola non è palindroma.";
+        risultatoPalindroma = "no";
     }
     console.log(parola);
+    return risultatoPalindroma;
 }
 
 // Inizio pari e dispari
@@ -24,45 +33,41 @@ const inputNumber = document.getElementById("input-number");
 const outPutNumeroPc = document.getElementById("number-pc");
 const outPutFinale = document.getElementById("risultato-finale");
 document.getElementById("number-pc").style.color = "red";
+document.getElementById("risultato-finale").style.fontSize = "50px";
+document.getElementById("risultato-finale").style.color = "green";
 
-// creo una funzione che genera un numero random da 1 a 5 per il pc
+// aggiungo evento al bottone
+const button = document.getElementById("my-button");
+button.addEventListener('click', function(){
+    let numeroPlayer = parseInt(inputNumber.value);
+    if ((numeroPlayer > 0) && (numeroPlayer < 6)){
+        let numeroPc = randomNumber(1, 5);
+        let sommatoria = sommaNumeri(numeroPlayer, numeroPc);
+        outPutNumeroPc.innerHTML = numeroPc;
+        //dichiarazione vincitore
+        if ((sommatoria %2 == 0) && (inputPariDispari.value == "pari")){
+            outPutFinale.innerHTML = "Hai vinto";
+        }else if ((sommatoria %2 != 0) && (inputPariDispari.value == "dispari")){
+            outPutFinale.innerHTML = "Hai vinto";
+        }else{
+            outPutFinale.innerHTML = "Hai perso";
+        }
+    }else{
+        outPutFinale.innerHTML = "Numero inserito errato";
+    }
+});
+
+// la funzione restituisce un numero random fra un valore minimo e uno massimo
 function randomNumber(minimumValue, maximumValue){
     //outPutNumeroPc.innerHTML += Math.floor(Math.random()*((maximumValue + 1) - minimumValue) + minimumValue);
     let valorePc = Math.floor(Math.random()*((maximumValue + 1) - minimumValue) + minimumValue);
     return valorePc;
 }
 
-// somma i due numeri
+// la funzione restituisce la somma di due numeri
 function sommaNumeri(val1, val2){
     let somma = val1 + val2;
     return somma;
 }
-
-// aggiungo evento al bottone
-const button = document.getElementById("my-button");
-button.addEventListener('click', function(){
-    let numeroPc = randomNumber(1, 5);
-    let sommatoria;
-    let nPlayer = inputNumber.value;
-    console.log(numeroPc);
-    console.log(nPlayer);
-    sommatoria = sommaNumeri(nPlayer, numeroPc);
-    console.log(sommatoria);
-    outPutFinale.innerHTML = sommaNumeri;
-
-    /*
-    if ((sommaNumeri %2 == 0) && (inputPariDispari.value == "pari")){
-        outPutFinale.innerHTML = "Hai vinto";
-    }else if ((sommaNumeri %2 != 0) && (inputPariDispari.value == "dispari")){
-        outPutFinale.innerHTML = "Hai vinto";
-    }else{
-        outPutFinale.innerHTML = "Hai perso";
-    }
-     */
-
-});
-
-
-
 
 
